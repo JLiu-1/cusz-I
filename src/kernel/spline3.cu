d3dc3766e1c0dab2cced4ed8d668774904275efe
meso272 <liujinyang1201@126.com>
2023-10-29 17:27:20 -0700

    auto-tuning-1-kernel

--- src/kernel/spline3.cu
+++ src/kernel/spline3.cu
@@ -67,11 +67,7 @@ int spline_construct(
 
 
 
- cusz::c_spline3d_profiling_32x8x8data<T*, DEFAULT_BLOCK_SIZE>  //
-      <<<grid_dim, dim3(DEFAULT_BLOCK_SIZE, 1, 1), 0, (GpuStreamT)stream>>>(
-          data->dptr(), data->template len3<dim3>(),
-          data->template st3<dim3>(),  //
-          profiling_errors->dptr());
+
 
 
 
